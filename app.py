4e266271a05f0d16cc1609e63f8f4e667ca72914
Marcela DL <leon.ari00@hotmail.com>
2024-04-04 20:41:27 +0200

    adding sqldb

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "myapp123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name= db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
 
 @app.route("/", methods = ["GET","POST"])
 def index():
@@ -10,10 +25,17 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
 
 if __name__ =="__main__":
-    app.run(debug=True, port=5001)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
